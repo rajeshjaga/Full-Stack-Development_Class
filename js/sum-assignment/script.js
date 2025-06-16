11edd2ff60a8af5edf9799e887c2b317732accba
Rajesh Jaga <rajaga223@gmail.com>
2025-06-16 12:56:29 +0530

    Commit: assignment7 and js assignment

---
+++ js/sum-assignment/script.js
@@ -0,0 +1,7 @@
+function submit() {
+    let a = parseInt(prompt("Please enter variable a"))
+    let b = parseInt(prompt("Please enter variable b"))
+
+    var sum = a + b;
+    alert(`the sum of a and b is: ${sum}`)
+}
