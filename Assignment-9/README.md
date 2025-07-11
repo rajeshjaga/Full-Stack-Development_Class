2e97274192d04aad85064297c3a7e797b347a26e
rajeshjaga <rajaga223@gmail.com>
2025-07-11 20:00:00 +0530

    commit: Adding assignment 9 from fsd class

---
+++ Assignment-9/README.md
@@ -0,0 +1,9 @@
+# Assignment 9
+
+This is an javascript module system assignment
+
+Use the below command to run the app:
+
+```bash
+npm run dev
+```
---
+++ Assignment-9/app.js
@@ -0,0 +1,6 @@
+import { add, subtract } from './math.js';
+import sayHello from './greet.js';
+
+console.log("Add:", add(10, 5));
+console.log("Subtract:", subtract(10, 5));
+console.log(sayHello("Rajesh"));
